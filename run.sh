629e770a19d2963a94d9e80ab500258d8ae3b037
Ren,Xie <LuVx21@users.noreply.github.com>
2022-06-22 14:56:43 +0800

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
+# sudo curl -L "https://github.com/docker/compose/releases/download/v2.6.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://get.daocloud.io/docker/compose/releases/download/v2.6.0/docker-compose-`uname -s`-`uname -m`" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
 
-
+# -------------------------------
 
 docker run -itd --name rustpad \
 -p 3030:3030 ekzhang/rustpad
