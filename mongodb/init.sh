d514a76978abfbe7eec80fb4751fb571341f1bd4
F.LuVx <yeyu0926@hotmail.com>
2024-03-01 19:32:13 +0800

    update

---
+++ mongodb/init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p $HOME/docker/mongo/mongo_{1,2,3}/{data,configdb}
+
+openssl rand -base64 756 > mongodb.key
+chmod 400 mongodb.key
+# mv mongodb.key $HOME/docker/mongo/mongo_{1,2,3}
+mv mongodb.key $HOME/docker/mongo
+
+docker exec -it mongo_1 mongosh "mongodb://admin:1121@localhost:27017/admin"
+
+# rs.initiate({
+#     _id: "rs0",
+#     members: [
+#         { _id : 0, host : "mini.rx:37017" },
+#         { _id : 1, host : "mini.rx:37018" },
+#         { _id : 2, host : "mini.rx:37019" }
+#     ]
+# });
