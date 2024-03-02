13d63ec73d921e1bd3e99a177a97fb949c4b1a64
F.LuVx <yeyu0926@hotmail.com>
2024-03-03 01:58:53 +0800

    mongodb shard net name

--- mongodb/shard.sh
+++ mongodb/shard.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-docker exec -it config_server bash -c "echo 'rs.initiate({_id: \"configRS\",configsvr: true, members: [{ _id : 0, host : \"192.168.31.201:27019\" }]})' | mongosh --port 27019"
+docker exec -it shard_config bash -c "echo 'rs.initiate({_id: \"configRS\",configsvr: true, members: [{ _id : 0, host : \"shard_config:27019\" }]})' | mongosh --port 27019"
 
 
-docker exec -it shard_rs00 bash -c "echo 'rs.initiate({_id: \"RS0\",members: [{ _id : 0, host : \"192.168.31.211:27018\" },{ _id : 1, host : \"192.168.31.212:27018\" }]})' | mongosh --port 27018"
-docker exec -it shard_rs10 bash -c "echo 'rs.initiate({_id: \"RS1\",members: [{ _id : 0, host : \"192.168.31.213:27018\" },{ _id : 1, host : \"192.168.31.214:27018\" }]})' | mongosh --port 27018"
+docker exec -it shard_rs00 bash -c "echo 'rs.initiate({_id: \"RS0\",members: [{ _id : 0, host : \"shard_rs00:27018\" },{ _id : 1, host : \"shard_rs01:27018\" }]})' | mongosh --port 27018"
+docker exec -it shard_rs10 bash -c "echo 'rs.initiate({_id: \"RS1\",members: [{ _id : 0, host : \"shard_rs10:27018\" },{ _id : 1, host : \"shard_rs11:27018\" }]})' | mongosh --port 27018"
 
+sleep 5
 
-docker exec -it shard_mongos bash -c "echo 'sh.addShard(\"RS0/192.168.31.211:27018,192.168.31.212:27018\")' | mongosh --port 27017"
-docker exec -it shard_mongos bash -c "echo 'sh.addShard(\"RS1/192.168.31.213:27018,192.168.31.214:27018\")' | mongosh --port 27017"
+docker exec -it shard_mongos bash -c "echo 'sh.addShard(\"RS0/shard_rs00:27018,shard_rs01:27018\")' | mongosh --port 27017"
+docker exec -it shard_mongos bash -c "echo 'sh.addShard(\"RS1/shard_rs10:27018,shard_rs11:27018\")' | mongosh --port 27017"
 
 # docker exec -it shard_mongos mongosh --port 27017
 # sh.shardCollection("test.testc", { _id : "hashed" } )
