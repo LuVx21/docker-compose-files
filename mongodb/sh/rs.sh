25341034a58ddac04ced3929856b60fb039ed59a
F.LuVx <yeyu0926@hotmail.com>
2024-03-03 13:27:40 +0800

    update

--- mongodb/sh/rs.sh
+++ mongodb/sh/rs.sh
@@ -7,13 +7,14 @@ chmod 400 mongodb.key
 # mv mongodb.key $HOME/docker/mongo/mongo_{1,2,3}
 mv mongodb.key $HOME/docker/mongo
 
-docker exec -it rs_mongo_1 mongosh "mongodb://admin:1121@localhost:27017/admin"
+docker exec -it rs_mongo_1 bash -c "echo 'rs.initiate({_id: \"rs0\",members: [{_id: 0, host: \"mini.rx:37017\"},{_id: 1, host: \"mini.rx:37018\"},{_id: 2, host: \"mini.rx:37019\"}]})' | mongosh \"mongodb://admin:1121@localhost:27017/admin\""
 
+# docker exec -it rs_mongo_1 mongosh "mongodb://admin:1121@localhost:27017/admin"
 # rs.initiate({
 #     _id: "rs0",
 #     members: [
-#         { _id : 0, host : "mini.rx:37017" },
-#         { _id : 1, host : "mini.rx:37018" },
-#         { _id : 2, host : "mini.rx:37019" }
+#         {_id: 0, host: "mini.rx:37017"},
+#         {_id: 1, host: "mini.rx:37018"},
+#         {_id: 2, host: "mini.rx:37019"}
 #     ]
 # });
--- mongodb/sh/shard.sh
+++ mongodb/sh/shard.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-docker compose -f docker-compose-shard.yml up -db
+docker compose -f ../docker-compose-shard.yml up -d
 
-sleep 30
+sleep 15
 
-docker exec -it shard_config bash -c "echo 'rs.initiate({_id: \"configRS\",configsvr: true, members: [{ _id : 0, host : \"shard_config:27019\" }]})' | mongosh --port 27019"
+docker exec -it shard_config_1 bash -c "echo 'rs.initiate({_id: \"configRS\",configsvr: true, members: [{ _id: 0, host: \"shard_config_1:27019\" },{ _id: 1, host: \"shard_config_2:27019\" }]})' | mongosh --port 27019"
 
 
-docker exec -it shard_rs00 bash -c "echo 'rs.initiate({_id: \"RS0\",members: [{ _id : 0, host : \"shard_rs00:27018\" },{ _id : 1, host : \"shard_rs01:27018\" }]})' | mongosh --port 27018"
-docker exec -it shard_rs10 bash -c "echo 'rs.initiate({_id: \"RS1\",members: [{ _id : 0, host : \"shard_rs10:27018\" },{ _id : 1, host : \"shard_rs11:27018\" }]})' | mongosh --port 27018"
+docker exec -it shard_00_00 bash -c "echo 'rs.initiate({_id: \"shard_00\",members: [{ _id: 0, host: \"shard_00_00:27018\" },{ _id: 1, host: \"shard_00_01:27018\" }]})' | mongosh --port 27018"
+docker exec -it shard_01_00 bash -c "echo 'rs.initiate({_id: \"shard_01\",members: [{ _id: 0, host: \"shard_01_00:27018\" },{ _id: 1, host: \"shard_01_01:27018\" }]})' | mongosh --port 27018"
 
-sleep 5
+sleep 15
 
-docker exec -it shard_router bash -c "echo 'sh.addShard(\"RS0/shard_rs00:27018,shard_rs01:27018\")' | mongosh --port 27017"
-docker exec -it shard_router bash -c "echo 'sh.addShard(\"RS1/shard_rs10:27018,shard_rs11:27018\")' | mongosh --port 27017"
+docker exec -it shard_router bash -c "echo 'sh.addShard(\"shard_00/shard_00_00:27018,shard_00_01:27018\")' | mongosh --port 27017"
+docker exec -it shard_router bash -c "echo 'sh.addShard(\"shard_01/shard_01_00:27018,shard_01_01:27018\")' | mongosh --port 27017"
 
 # docker exec -it shard_router mongosh --port 27017
-# sh.shardCollection("test.testc", { _id : "hashed" } )
+# sh.shardCollection("test.testc", { _id: "hashed" } )
 # for(i=0; i<1000;i++) {db.testc.insertOne({_id: i, "name": "User_" + i});}
 # db.testc.find().count()
 
-# docker exec -it shard_rs00 mongosh --port 27018
+# docker exec -it shard_00_00 mongosh --port 27018
 # db.testc.countDocuments()
