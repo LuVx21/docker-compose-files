9056389d406bba2be7eea9057f1d98ae470b01ec
F.LuVx <yeyu0926@hotmail.com>
2024-09-12 12:38:36 +0800

    coder

--- luvx/build.sh
+++ luvx/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 curl https://raw.githubusercontent.com/alibaba/Sentinel/master/sentinel-dashboard/Dockerfile > ./sentinel-dashboard/Dockerfile
-cd sentinel-dashboard && sed -i "" 's/1.8.6/1.8.8/g' Dockerfile && docker buildx build -t luvx/sentinel-dashboard:latest --platform linux/amd64,linux/arm64 . --push
+cd sentinel-dashboard && sed -i "" 's/1.8.6/1.8.8/g' Dockerfile && docker buildx build -t luvx/sentinel-dashboard:latest -t luvx/sentinel-dashboard:1.8.8 --platform linux/amd64 . --push
+
+git clone --depth=1 https://github.com/chartdb/chartdb
+cd chartdb && docker buildx build -t luvx/chartdb --platform linux/amd64,linux/arm64 . --push
+
+git clone --depth=1 https://github.com/zyx0814/Pichome-docker pichome
+cd pichome && docker buildx build -t luvx/pichome:latest -t luvx/pichome:2.0 --platform linux/amd64,linux/arm64 . --push
